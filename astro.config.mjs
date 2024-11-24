fda3f63362ed0e7391c85bdd6c6be027aa6d29c2
Nick Ruffin <nruffinsecure@gmail.com>
2024-11-24 11:57:07 -0600

    updates to handle server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
+  adapter: cloudflare({
+    mode: 'directory'
+  }),
   server: {
     port: 4321,
   },
